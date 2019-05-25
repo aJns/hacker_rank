d28853d8a8aa79bf429d6579be491870b7dfb13d
Jonas Nikula <nigula77@gmail.com>
2019-05-25 21:05:10 +0300

    1: printout 2 stdin lines

--- interview_prep_kit/1/main.lisp
+++ interview_prep_kit/1/main.lisp
@@ -1,2 +1,7 @@
 ;; Enter your code here. Read input from STDIN. Print output to STDOUT
 
+(progn
+  (let
+      ((eka-rivi (read-line))
+       (toka-rivi (read-line)))
+    (format t "~a~%~a~%" eka-rivi toka-rivi)))
