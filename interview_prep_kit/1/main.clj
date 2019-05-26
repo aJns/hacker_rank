dc372d943ea84f68110334cf69a6ebeb20850b56
Jonas Nikula <jonas.nikula@gmail.com>
2019-05-26 21:18:53 +0300

    1: solve with clojure

--- interview_prep_kit/1/main.clj
+++ interview_prep_kit/1/main.clj
@@ -1,7 +1,6 @@
 ; Complete the sockMerchant function below.
 (defn sockMerchant [n ar]
-
-
+  (reduce + (map (fn [x] (quot x 2)) (vals (frequencies [1 2 3 4 1 1 2 4]))))
 )
 
 (def fptr (get (System/getenv) "OUTPUT_PATH"))
