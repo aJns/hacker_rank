3b25b35dfb36621ad78b7073e4c105d83489629f
Jonas Nikula <jonas.nikula@gmail.com>
2019-06-28 01:01:44 +0300

    Add stub of 4

---
+++ interview_prep_kit/4/main.py
@@ -0,0 +1,14 @@
+import os
+
+import common
+
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    return 0
+
+
+if __name__ == '__main__':
+    input_path = os.path.abspath("testdata/input.txt")
+    output_path = os.path.abspath("testdata/output.txt")
+    common.test_solution(repeatedString, input_path, output_path)
